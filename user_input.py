5172dd4e4ec1e2dd07c4bda512334d394b7079d4
Socialstranger <117745220+Socialstranger@users.noreply.github.com>
2024-08-15 14:45:09 +0300

    added caps

--- user_input.py
+++ user_input.py
@@ -1,4 +1,4 @@
-name = str(input('enter your name:'))
-age = int(input('enter your age:'))
+name = str(input('Enter your name:'))
+age = int(input('Enter your age:'))
 location = str(input('enter your location:'))
 print(f'hello {name} welcome to powerlearn project')
